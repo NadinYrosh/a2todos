e7b71d0b5905da3d104b22ac6d0bddef8806306f
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2016-12-06 19:21:02 -0800

    forLoop through the toDos and be able to delete them

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos;
+  text;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todos = [
+      {
+        text: 'Pick up apples'
+      },
+      {
+        text: 'Do leandry'
+      },
+      {
+        text: 'Study an'
+      }
+    ];
+  }
+
+  addTodo(){
+    this.todos.push({
+      text: this.text
+    });
+  }
+
+  deleteTodo(todoText){
+    for(var i = 0; i < this.todos.length; i++){
+      if(this.todos[i].text == todoText){
+        this.todos.splice(i, 1);
+      }
+    }
+  }
+}
